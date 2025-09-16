a2f9f2db74e000d16a3f81b7838a29a421b9628f
Supun Adikari <adikariamssh@gmail.com>
2025-09-16 15:51:21 +0530

    user modal added

---
+++ src/main/java/com/shopify/store/entities/User.java
@@ -0,0 +1,40 @@
+package com.shopify.store.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor 
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false,name="name")
+    private String name;
+
+    @Column(nullable = false,name="email")
+    private String email;
+
+    @Column(nullable = false,name="password")
+    private String password;
+
+   
+        
+}
