3697e1d36afa06e3b0ce891d2b2a26b3f03ae605
Supun Adikari <adikariamssh@gmail.com>
2025-09-16 16:34:26 +0530

    Tag modal added

---
+++ src/main/java/com/shopify/store/entities/Tag.java
@@ -0,0 +1,32 @@
+package com.shopify.store.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor 
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "tags")
+
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+    
+}
